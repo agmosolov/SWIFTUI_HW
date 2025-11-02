2df766e39096f087de56f8023fee1b90711346fb
Aleksandr Mosolov <agmosolov@gmail.com>
2025-11-02 15:59:36 +0300

    Initial Commit

---
+++ SWIFTUI_HW/SWIFTUI_HWApp.swift
@@ -0,0 +1,17 @@
+//
+//  SWIFTUI_HWApp.swift
+//  SWIFTUI_HW
+//
+//  Created by Александр Мосолов on 02.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct SWIFTUI_HWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
